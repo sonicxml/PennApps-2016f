93b209a24fc4a5a9ec3d93667e4d8b2262df9b1f
Trevin Gandhi <sonicxml@gmail.com>
2016-09-10 15:10:28 -0400

    Throw exception when token is invalid

--- server.py
+++ server.py
@@ -21,7 +21,18 @@ def get_restaurants_from_yelp(location):
 @app.route('/slack', methods=['POST'])
 def generate_response():
 	form_info = flask.request.form
+
+	if config.API_TOKEN != form_info['token']:
+		raise InvalidTokenException
+
 	# First, parse args and find the restaurants
 	passed_args = form_info['text']
 	restaurants = get_restaurants_from_yelp(config.LOCATION)
 	# Next, create poll for people to vote on the restaurants
+
+def main():
+
+
+
+if __name__ == '__main__':
+	main()
