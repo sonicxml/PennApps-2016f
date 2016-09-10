7b71e4051ee0e6b02a022c598c42c7c72603de8f
Trevin Gandhi <sonicxml@gmail.com>
2016-09-10 12:57:14 -0400

    Start setting up Flask server for slack app

---
+++ server.py
@@ -0,0 +1,12 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return '83'
+
+@app.route('/slack', methods=['POST'])
+def get_restaurants_from_yelp():
+	# Talk to yelp
+	return restaurants
